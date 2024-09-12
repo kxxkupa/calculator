e5e53662385daf6ab540a7335dc88d13a41ac161
kxxkupa <kxxkupa@gmail.com>
2024-09-12 11:44:30 +0900

    Add icon.png file and push button with handler functionin main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import sys # 시스템 제어 관련 모듈
 
 # 위젯이란 : GUI 프로그램에서 구성요소를 뜻하는 용어
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,QMessageBox)
+from PyQt5.QtGui import QIcon
 
 # 나는 계산기 유형을 직접 정의한다. 이때, QWidget에 기반을 둔다.
 class Calculator(QWidget) :
@@ -11,9 +12,27 @@ class Calculator(QWidget) :
         self.initUI()
 
     def initUI(self) :
-        self.setWindowTitle('Calculator')
+        self.btn1 = QPushButton("Message", self)
+
+        # 이벤트 핸들링 : 클릭했을 때, 뭐를 할거다! 라고 정하는 것
+        self.btn1.clicked.connect(self.activateMessage)
+
+        # 레이아웃 설정
+        vbox = QVBoxLayout()
+        vbox.addStretch(1) # 여백
+        vbox.addWidget(self.btn1) # 버튼
+        vbox.addStretch(1) # 여백
+
+        self.setLayout(vbox) # 설정 적용
+
+        self.setWindowTitle("Calculator")
+        self.setWindowIcon(QIcon("icon.png"))
         self.resize(256, 256)
         self.show()
+
+    def activateMessage(self) :
+        QMessageBox.information(self, "information", "Button clicked!")
+
 
 # 클래스를 정의했으니, 여기에서 실행하겠다. 라는 실행부
 # if __name__ == "__main__" : 이 모듈이 직접 실행되는 경우
